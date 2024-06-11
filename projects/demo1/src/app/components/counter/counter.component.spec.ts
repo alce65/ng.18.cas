8fa7caf94b1b5d2dcc7e0036a662bdd92c38c361
Alejandro Cerezo <alce65@hotmail.es>
2024-06-11 17:52:38 +0200

    Update testing environment and test components

--- projects/demo1/src/app/components/counter/counter.component.spec.ts
+++ projects/demo1/src/app/components/counter/counter.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CounterComponent } from './counter.component';
+import { By } from '@angular/platform-browser';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
@@ -17,5 +18,31 @@ describe('CounterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change count', () => {
+    const spanElement = fixture.debugElement.query(By.css('span'))
+      .nativeElement as HTMLSpanElement;
+    expect(spanElement.textContent).toBe('0');
+    fixture.debugElement.queryAll(By.css('button'))[0].nativeElement.click();
+    fixture.detectChanges();
+    expect(spanElement.textContent).toBe('-1');
+    fixture.debugElement.queryAll(By.css('button'))[1].nativeElement.click();
+    fixture.detectChanges();
+    expect(spanElement.textContent).toBe('0');
+  });
+
+  it('should not change count in the lower limit', () => {
+    component.count = -5;
+    expect(component.count).toBe(-5);
+    component.change(-1);
+    expect(component.count).toBe(-5);
+  });
+
+  it('should not change count in the upper limit', () => {
+    component.count = 5;
+    expect(component.count).toBe(5);
+    component.change(1);
+    expect(component.count).toBe(5);
   });
 });
--- projects/demo1/src/app/components/greetings/greetings.component.spec.ts
+++ projects/demo1/src/app/components/greetings/greetings.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GreetingsComponent } from './greetings.component';
+import { By } from '@angular/platform-browser';
 
 describe('GreetingsComponent', () => {
   let component: GreetingsComponent;
@@ -8,9 +8,8 @@ describe('GreetingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GreetingsComponent]
-    })
-    .compileComponents();
+      imports: [GreetingsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GreetingsComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,38 @@ describe('GreetingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  xdescribe('White box test', () => {
+    it('should handle click', () => {
+      component.user = 'Pepe';
+      expect(component.user).toBe('Pepe');
+      component.handleClick();
+      expect(component.user).toBe('');
+    });
+  });
+
+  describe('Black box test', () => {
+    it('should render input', () => {
+      const inputElement = fixture.debugElement.query(By.css('input'))
+        .nativeElement as HTMLInputElement;
+      const pElement = fixture.debugElement.query(By.css('p'))
+        .nativeElement as HTMLParagraphElement;
+      inputElement.value = 'Pepe';
+      inputElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(pElement.textContent).toContain('Pepe');
+    });
+
+    it('should handle click', () => {
+      const buttonElement = fixture.debugElement.query(By.css('button'))
+        .nativeElement as HTMLButtonElement;
+      const pElement = fixture.debugElement.query(By.css('p'))
+        .nativeElement as HTMLParagraphElement;
+      component.user = 'Pepe';
+      buttonElement.click();
+      fixture.detectChanges();
+      expect(pElement.textContent).toContain('amigo');
+    });
   });
 });
