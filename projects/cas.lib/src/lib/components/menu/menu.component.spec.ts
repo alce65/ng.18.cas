a83ae7c76b5218732b766f2f7499ba12ea9d3c34
Alejandro Cerezo <alce65@hotmail.es>
2024-06-17 16:39:11 +0200

    Update test config nd test in cas lib

--- projects/cas.lib/src/lib/components/menu/menu.component.spec.ts
+++ projects/cas.lib/src/lib/components/menu/menu.component.spec.ts
@@ -1,18 +1,18 @@
 import {
   ComponentFixture,
-  TestBed,
-  fakeAsync,
-  tick,
+  TestBed
 } from '@angular/core/testing';
 import { MenuComponent } from './menu.component';
 import { MenuOptions } from '../../types/menu.options.type';
-import { routes } from '../../../app.routes';
+
 import { By } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
-const menuOptions: MenuOptions[] = routes
-  .filter((r) => r.title)
-  .map((r) => ({ title: r.title as string, path: r.path as string }));
+const menuOptions: MenuOptions[] = [
+  { title: 'Home', path: '/home' },
+  { title: 'Todo', path: '/todo' },
+  { title: 'About', path: '/about' },
+];
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
@@ -21,7 +21,7 @@ describe('MenuComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MenuComponent],
-      providers: [provideRouter(routes)],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
@@ -36,42 +36,12 @@ describe('MenuComponent', () => {
 
   it('should render menu options', () => {
     const debugLinks = fixture.debugElement.queryAll(By.css('a'));
-    expect(debugLinks.length).toBe(menuOptions.length);
     const linkElements = debugLinks.map(
       (debugElement) => debugElement.nativeElement,
     );
+    expect(linkElements.length).toBe(menuOptions.length);
     linkElements.forEach((linkElement, index) => {
       expect(linkElement.textContent).toBe(menuOptions[index].title);
-      // expect(linkElement.getAttribute('routerLink')).toBe(
-      //   menuOptions[index].path,
-      // );
     });
-  });
-
-  describe('MenuComponent', () => {
-    let linkElements: HTMLAnchorElement[];
-    beforeEach(() => {
-      const debugLinks = fixture.debugElement.queryAll(By.css('a'));
-      linkElements = debugLinks.map(
-        (debugElement) => debugElement.nativeElement,
-      );
-    });
-
-    it('should have select route home', fakeAsync(() => {
-      linkElements[0].click();
-      tick();
-      expect(linkElements[0].classList).toContain('active');
-      expect(linkElements[0]).toHaveClass('active');
-    }));
-    it('should have select route todo', fakeAsync(() => {
-      linkElements[1].click();
-      tick();
-      expect(linkElements[1]).toHaveClass('active');
-    }));
-    it('should have select route about', fakeAsync(() => {
-      linkElements[2].click();
-      tick();
-      expect(linkElements[2]).toHaveClass('active');
-    }));
   });
 });
